e535c1c1a75496a421ca4c138a11d81d56df52f6
Sia <siakaramalegos@gmail.com>
2023-08-04 17:26:52 -0400

    ✨ [FEATURE] Override response headers to allow websites that block iframes to be linted (#4)
    * Override response headers to allow websites that block iframes to be linted
    * Clean up promise

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,13 +1,60 @@
 import { initCollector } from './scripts/collector'
 
+async function configureNetRequest(tabId, domain) {
+  const domains = [domain];
+  const headers = [
+    "X-Frame-Options",
+    "Frame-Options",
+    "Content-Security-Policy",
+  ];
+
+  await chrome.declarativeNetRequest.updateSessionRules({
+    removeRuleIds: [1],
+    addRules: [
+      {
+        id: 1,
+        action: {
+          type: "modifyHeaders",
+          responseHeaders: headers.map((h) => ({
+            header: h,
+            operation: "remove",
+          })),
+        },
+        condition: {
+          requestDomains: domains,
+          resourceTypes: ["sub_frame"],
+          tabIds: [tabId],
+        },
+      },
+    ],
+  });
+
+  await chrome.browsingData.remove(
+    {
+      origins: domains.map((d) => `https://${d}`),
+    },
+    {
+      cacheStorage: true,
+      serviceWorkers: true,
+    }
+  );
+}
+
 /**
  * Run the collector script when the extension is clicked
  * @param {chrome.tabs.Tab} tab The current tab the user is on
  */
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
+  const domain = new URL(tab.url).hostname;
+
+  // Remove response headers that block iframes
+  await configureNetRequest(tab.id, domain);
+
   // Inject the collector script into the current tab
-  chrome.scripting.executeScript({
+  await chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: initCollector,
-  }).then(() => console.log('Script injected'))
+  })
+
+  console.log('Script injected')
 })
